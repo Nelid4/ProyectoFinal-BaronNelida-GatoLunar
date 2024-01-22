b66e38bdccf99a1d66eeaaca786dc59fcc66406c
Neli <nelidabaron@gmail.com>
2024-01-22 19:13:07 -0300

    Añadimos un boton con lista de clickeables

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,20 +3,36 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CartWidget from '../CartWidget/CartWidget';
+import Dropdown from 'react-bootstrap/Dropdown';
 import Logo from '../Logo/Logo';
 import './NavBar.css'
 
 function NavBar() {
   return (
     <>
+{/* Barra de menú */}
       <Navbar className='bg'>
         <Container>
-          <Navbar.Brand href="#home"> <Logo/> </Navbar.Brand>
+          <Navbar.Brand href="#Home-gatuna"> <Logo/> </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home" className='textoNav'>Todos los libros</Nav.Link>
-            <Nav.Link href="#features" className='textoNav'>Libros nuevos</Nav.Link>
-            <Nav.Link href="#pricing" className='textoNav'>Libros usados</Nav.Link>
-            <Nav.Link href="#pricing" className='paddingCarrito'> <CartWidget /> </Nav.Link>
+            <Nav.Link href="#Todos-los-libros" className='textoNav'>Todos los libros</Nav.Link>
+            <Nav.Link href="#Libros-nuevos" className='textoNav'>Libros nuevos</Nav.Link>
+            <Nav.Link href="#Libros-Usados" className='textoNav'>Libros usados</Nav.Link>
+{/* Botón con listado de categorías clickeables*/}
+            <Nav.Link><Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Géneros Literarios
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#Romance">Romance</Dropdown.Item>
+        <Dropdown.Item href="#Thriller">Thriller</Dropdown.Item>
+        <Dropdown.Item href="#Biografias">Biografias</Dropdown.Item>
+        <Dropdown.Item href="#Fantasia">Fantasia</Dropdown.Item>
+{/* Botón de carrito, CartWidget */}
+      </Dropdown.Menu>
+    </Dropdown></Nav.Link>
+            <Nav.Link href="#carrito" className='paddingCarrito'> <CartWidget /> </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
