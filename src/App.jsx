ddafa1dd664166f531204a84ae9c2b7a6e348a52
Neli <nelidabaron@gmail.com>
2024-03-02 19:51:54 -0300

    se agregó un carrusel desde bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,62 @@
-import React from 'react'
-import NavBar from './Components/NavBar/NavBar'
-import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
-import './App.css'
-import{BrowserRouter, Routes, Route} from "react-router-dom"
-import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer'
-import Footer from './Components/Footer/Footer'
+import React, { useState, useEffect } from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
+import Footer from './Components/Footer/Footer';
 
 function App() {
+  const [showCarousel, setShowCarousel] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowCarousel(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <BrowserRouter>
-    <NavBar />
+      <NavBar />
+      {showCarousel && (
+        <div className="carrusel">
+          <div id="carouselExample" className="carousel slide">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src="./src/imagenes/BANNER4.png" className="d-block w-100" alt="BANNER" />
+              </div>
+              <div className="carousel-item">
+                <img src="./src/imagenes/BANNER2.png" className="d-block w-100" alt="BANNER" />
+              </div>
+              <div className="carousel-item">
+                <img src="./src/imagenes/BANNER3.png" className="d-block w-100" alt="BANNER" />
+              </div>
+              <div className="carousel-item">
+                <img src="./src/imagenes/BANNER1.png" className="d-block w-100" alt="BANNER" />
+              </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      )}
       <Routes>
-        <Route path="/" element= {<ItemListContainer/>}/>
-        <Route path='/condition/:condition' element={<ItemListContainer/>}/>
-        <Route path='/genre/:genre' element={<ItemListContainer/>}/>
-        <Route path='/item/:id' element={<ItemDetailContainer/>}/>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path='/condition/:condition' element={<ItemListContainer />} />
+        <Route path='/genre/:genre' element={<ItemListContainer />} />
+        <Route path='/item/:id' element={<ItemDetailContainer />} />
       </Routes>
-        <Footer />
-
+      <Footer />
     </BrowserRouter>
   );
 }
-
-
-//     <div>
-
-// {/* MOSTRAR PERSONAJES; CON .MAP Y .FILTER */}
-//       {/* <ItemListContainer/> */}
-
-// {/* MOSTRAR PERSONAJES CON NOMBRE Y BOTON NEXT AND PREVIOUS */}
-//       {/* <PersonajesApi/> */}
-// {/* CONTADOR */}
-//     <Contador />
-// <br />
-// {/* CARD CON LOS LIBROS */}
-//       {/* <CardBooks img='https://images.cdn1.buscalibre.com/fit-in/360x360/ff/9e/ff9ea4db43eb1ec258983f18c9a384bd.jpg' 
-//       title='Un cuento perfecto' 
-//       genre='Romance' 
-//       price= {15000} />
-
-//       <CardBooks img='https://www.tematika.com/media/catalog/Ilhsa/Imagenes/703582.jpg' 
-//       title='Me alegro de que mi madre haya muerto' 
-//       genre='Biografia' 
-//       price={13500} />
-
-//       <CardBooks img='https://images.cdn2.buscalibre.com/fit-in/360x360/52/fb/52fbe2c8719e8c0526a9dcd2d7e1b9ad.jpg' 
-//       title='Cadáver exquisito' 
-//       genre='Thriller' 
-//       price={11000} /> */}
-
-    // </div>
 
 export default App;
